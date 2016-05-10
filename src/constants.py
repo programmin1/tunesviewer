8598cb0417442916094ec3877e581adadd7eda73
lb1programmer <lb1programmer@users.sourceforge.net>
2016-05-09 21:44:08 -0700

    Fix packaging for newer Ubuntu/Debian

--- src/constants.py
+++ src/constants.py
@@ -25,7 +25,7 @@ import glib
 
 # Path of the program (bad assumption)
 TV_PATH = "/usr/bin/tunesviewer"
-TV_VERSION = "1.5.3" #also needs changing in debian conf file somewhere
+TV_VERSION = "1.5.4" #also needs changing in debian/changelog
 
 # Directory under which we write configuration files
 USER_PREFS_DIR = glib.get_user_config_dir()
